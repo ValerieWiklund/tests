da482726a6082932c252de71bb0eb813bfad2191
Valerie Wiklund <vkwiklund@gmail.com>
2020-01-07 17:55:38 -0700

    added meridiem

--- script.js
+++ script.js
@@ -3,6 +3,16 @@ function displayTime() {
   let hours = currentTime.getHours();
   let minutes = currentTime.getMinutes();
   let seconds = currentTime.getSeconds();
+  let meridiem = "AM";
+  if (hours > 11) {
+    meridiem = "PM";
+  }
+  if (hours > 12) {
+    hours = hours - 12;
+  }
+  if (hours === 0) {
+    hours = 12;
+  }
   if (seconds < 10) {
     seconds = "0" + seconds;
   }
@@ -12,8 +22,9 @@ function displayTime() {
   if (hours < 10) {
     hours = "0" + hours;
   }
+
   let clock = document.querySelector("#clock");
-  clock.innerText = hours + ":" + minutes + ":" + seconds;
+  clock.innerText = hours + ":" + minutes + ":" + seconds + " " + meridiem;
 }
 displayTime();
 setInterval(displayTime, 1000);
